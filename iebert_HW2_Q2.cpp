04dc9a213bc282f7898ffb8a94b294644398001b
MoonPanda134 <93020315+MoonPanda134@users.noreply.github.com>
2021-10-22 22:48:23 -0400

    Add files via upload

---
+++ iebert_HW2_Q2.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+** Author : Isaiah Ebert
+** Program : hw2, q2
+** Date Created : October 22, 2021
+** Date Last Modified : October 22, 2021
+** Usage : No command line arguments
+**
+** Problem:
+Create a struct called 'Check' and a class called 'CheckBook'.
+CheckBook contains an array of 'Checks' write checks until their
+value doesn't allow for anymore through 'balance'. Display all 
+the 'Checks'.
+*************************************************************************/
+
+#include <iostream>
+
